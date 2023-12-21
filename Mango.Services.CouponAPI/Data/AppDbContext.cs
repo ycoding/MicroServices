b70d8d2b7739501b0e7199bd91db89c2d21472f6
Younes Hashlamoun <Younes.Engineer@Gmail.com>
2023-12-21 17:15:52 -0600

    CouponAPI CRDUD is completed

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -10,5 +10,25 @@ namespace Mango.Services.CouponAPI.Data
             
         }
         public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating( ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponID = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinAmount = 20,
+            });
+            
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponID = 2,
+                CouponCode = "20OFF",
+                DiscountAmount = 20,
+                MinAmount = 40,
+            });
+        }
     }
 }
--- Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54dbf39ec893270ef9bfe666f97bba1c2aeb27e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2292d9e4ff6af30f31591f75f5dc62e81bc74dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
